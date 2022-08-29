f95829cbdf3de39d01e737efbb578f59748320f5
Armanndeep Kaur <104866931+ArmanndeepKaur@users.noreply.github.com>
2022-08-29 13:35:07 +0000

    modified intro a little

--- intro/intro.py
+++ intro/intro.py
@@ -2,3 +2,19 @@
 
 print("Hello World!")
 print("line two")
+
+a = "this is a string variable"
+b = 23
+print(a)
+print(b)
+
+'''c = a + b
+print(c)'''
+# This will cause an error because we cannot add a string and an int together 
+
+a = 3
+b = a 
+print("a:", a, "b:", b) #use commas to separate 
+
+a = 25 '''an assignment telling to evaluate the right side and store that value in the memory reffered to by the name on the left hand side'''
+print("a:", a, "b:", b)
