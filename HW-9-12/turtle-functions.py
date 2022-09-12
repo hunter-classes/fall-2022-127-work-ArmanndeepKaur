60a1d792a74342d4fec260d4b1b31197f85f9b9b
Armanndeep Kaur <104866931+ArmanndeepKaur@users.noreply.github.com>
2022-09-12 01:18:52 +0000

    added ngon

--- HW-9-12/turtle-functions.py
+++ HW-9-12/turtle-functions.py
@@ -1,19 +1,37 @@
-#Draw a Hexagon with the same parameters as square and triangle 
-
 import turtle 
 
+#Draw a Hexagon with the same parameters as square and triangle 
 def hexagon(t, x, y, w, color, sidelen):
-
-  t.penup()
-  t.goto(0,0)
-  t.width(2)
-  t.color("#f37736")
-  t.pendown()
+   t.penup()
+   t.goto(0,0)
+   t.width(2)
+   t.color("#f37736")
+   t.pendown()
     
-  for i in range(6):
-    t.forward(sidelen)
-    t.left(60)
+   for i in range(6):
+     t.forward(sidelen)
+     t.left(60)
 
 wn = turtle.Screen()
 ann = turtle.Turtle()
 hexagon(ann, 0, 0, 2, "#f37736", 100)
+
+
+
+#Draw a ngon with parameters(t, numsides, x, y, color, width, sideln)
+def ngon(t, numsides, x, y, color, width, sidelen):
+    t.penup()
+    t.goto(23,23)
+    t.width(3)
+    t.color("blue")
+    t.pendown()
+    
+    for i in range(9):
+        t.forward(sidelen)
+        t.left(40)
+
+anna = turtle.Turtle()
+ngon(anna, 9, 23, 23, "blue", 3, 50)
+
+
+wn.mainloop()
