c0019b83af2d4669140480921cac753a6b9cf1d0
Armanndeep Kaur <104866931+ArmanndeepKaur@users.noreply.github.com>
2022-10-03 04:10:11 +0000

    added code to file 2 of chapter 10 folder

--- chapter-10/list2.py
+++ chapter-10/list2.py
@@ -5,3 +5,16 @@ def average(list):
   for i in list:
     number = number + 1
   return number / len(list) #--> getting the average 
+
+
+#Exercise 6 
+
+list = [2,3,4]
+def sum_of_squares(xs):
+  number = 0
+  for i in xs:
+    number = number ** 2 #--> squares 
+    # adding all of them together = ???
+  return number  
+  
+  
