f6ec91aec6a66c957bb67968b60c3ca28464daee
Armanndeep Kaur <104866931+ArmanndeepKaur@users.noreply.github.com>
2022-10-13 03:29:00 +0000

    added code

--- listlab.py
+++ listlab.py
@@ -1,4 +1,4 @@
-#Chpater 10 Exercise #10 --> Count how many words in a list have length 5 
+#Chapter 10 Exercise #10 --> Count how many words in a list have length 5 
 listA = ["Replit", "Lab", "CS", 12700, "Tablet", 5,"seven"]
 #print(listA)
 #print(len(listA))
@@ -7,5 +7,27 @@ count == 0
 for words in listA:
   if words == len(5)
   count = count+1
-print(count)  #couldn't get it to work even after trying several different methods 
+print(count)  #didn't work, work on it again (maybe try using a function) 
+
+
+#Chapter 10 Exercise #11 --> Sum all the elements in a list up to but not including the first even number 
+nums = [1,2,3,4,5,6,7,8]
+def adding_num(x):
+  sum = 0
+  for elements in nums:
+    if elements % 2 == 0:
+      sum = sum + 1
+    return sum   #test it 
+
+
+#Chapter 10 Exercise #12 --> Count how many words occur in a list up to and including the first occurance of the word 'sam'
+randoms ={"Anna", 92.3, "True", "Sam", 92.3, 76, 4, False, "hello"}
+#print(randoms)
+print(randoms.count('sam'))
+del randoms[4:]
+print(randoms)   #working 
+
+
+
+
 
