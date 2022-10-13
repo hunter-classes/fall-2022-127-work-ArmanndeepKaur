964edd9efca24d594a6510a8b45e61fa605e9eb2
Armanndeep Kaur <104866931+ArmanndeepKaur@users.noreply.github.com>
2022-10-13 02:49:55 +0000

    added a file and some code

---
+++ listlab.py
@@ -0,0 +1,11 @@
+#Chpater 10 Exercise #10 --> Count how many words in a list have length 5 
+listA = ["Replit", "Lab", "CS", 12700, "Tablet", 5,"seven"]
+#print(listA)
+#print(len(listA))
+#print(listA.count(5))
+count == 0
+for words in listA:
+  if words == len(5)
+  count = count+1
+print(count)  #couldn't get it to work even after trying several different methods 
+
