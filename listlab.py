056936b3dc6307f7610fa93334aa5cbe3b29beb0
Armanndeep Kaur <104866931+ArmanndeepKaur@users.noreply.github.com>
2022-10-23 03:18:39 +0000

    wrote the correct code

--- listlab.py
+++ listlab.py
@@ -1,45 +1,109 @@
-#Chapter 10 Exercise #10 --> Count how many words in a list have length 5 
-listA = ["Replit", "Lab", "CS", 12700, "Tablet", 5,"seven"]
-#print(listA)
-#print(len(listA))
-#print(listA.count(5))
-count == 0
+#Chapter 10 Exercise 10 --> Count how many words in a list have length 5 
+listA = ["Replit", "Lab", "CS", "Tablet", "seven"]
+count = 0
 for words in listA:
-  if words == len(5)
-  count = count+1
-print(count)  #didn't work, work on it again (maybe try using a function) 
+  if len(words) == 5:
+    count = count+1
+print(count)  
 
 
-#Chapter 10 Exercise #11 --> Sum all the elements in a list up to but not including the first even number 
-nums = [1,2,3,4,5,6,7,8]
+#Chapter 10 Exercise 11 --> Sum all the elements in a list up to but not including the first even number 
+'''nums = [1,2,3,4,5,6,7,8]
 def adding_num(x):
   sum = 0
   for elements in nums:
     if elements % 2 == 0:
       sum = sum + 1
-    return sum   #test it 
+    return sum'''
+
+def up_to_even(l):
+    result = []
+    i = 0
+    while l[i] % 2 != 0:
+        result.append(l[i])
+        i = i+1
+    return result
 
 
-#Chapter 10 Exercise #12 --> Count how many words occur in a list up to and including the first occurance of the word 'sam'
-randoms ={"Anna", 92.3, "True", "Sam", 92.3, 76, 4, False, "hello"}
-#print(randoms)
+#Chapter 10 Exercise 12 --> Count how many words occur in a list up to and including the first occurance of the word 'sam'
+'''randoms ={"Anna", 92.3, "True", "Sam", 92.3, 76, 4, False, "hello"}
 print(randoms.count('sam'))
 del randoms[4:]
-print(randoms)   #works 
+print(randoms)''' 
+
+def up_to_sam(l):
+    result = []
+    i = 0
+    while l[i] != "sam":
+        result.append(l[i])
+        i = i+1
+    return " ".join(result)
+
+#Most of my own codes for the lab were wrong or were not written as functions, therefore, I deleted them (learned where I made mistakes and how I can improve) and replaced them with codes from the github (because I will look back at these)
+
+#Function that finds the smallest value 
+def find_smallest(thelist):
+    small_so_far = thelist[0]
+    for item in thelist[1:]:
+        if item < small_so_far:
+            small_so_far = item
+    return small_so_far
+  
+
+#Function that returns a new list with all the odd items from the orginial list 
+def odd_items(l):
+  result = []
+  for items in l:
+    if items % 2 == 1:
+        result.append(items)
+  return result 
 
 
-#A function that return a new list that contains all the add items in the original list
-l = ["a", True, 99, "b" False, 100]
-def add(l):
-  sum = 0
+#Function that returns a string with captalized words 
+def capitalization():
+  result_list = []
+    for word in s.split():
+        result_list = result_list + [word.capitalize()]
+    result = " ".join(result_list) #learn how and why to use .join
+    return result
+
+
+#Function that returns a string with 5+ words uppercase 
+def long_5_cap(s):
+    result_list = []
+    for word in s.split():
+        if len(word) > 5:
+            result_list = result_list + [word.upper()]
+        else:
+            result_list = result_list + [word]
+    result = " ".join(result_list)
+    return result
+
+
+#Function that returns each item squared 
+def square_list(l):
+  result = []
   for item in l:
-    sum = sum + item
-  return sum 
+    #result = item**2
+    result.append(item*item)
+  return result
 
 
-#A function that returns the smallest value in a list 
-k = [1, -1, 20, 11, 8]
-print(min(k)) #write its function later 
+#Function that returns corresponding values and add the lists together 
+ef combine_sum(l1,l2):
+    result = []
+    if len(l1) < len(l2):
+        shorter = len(l1)
+    else:
+        shorter = len(l2)
+    for i in range(shorter):
+        result.append(l1[i] + l2[i])
+    if len(l1) > shorter:
+        result = result + l1[i:]
+    else:
+        result = result + l2[i:]
+    return result
+
 
 
 
