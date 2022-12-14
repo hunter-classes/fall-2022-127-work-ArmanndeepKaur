fc0f4424640d5b584067f3769cd6799e00b1e92a
Armanndeep Kaur <104866931+ArmanndeepKaur@users.noreply.github.com>
2022-12-14 11:53:50 +0000

    added the extra

--- data/data.py
+++ data/data.py
@@ -19,8 +19,28 @@ print("The range of XLarge bags:", df.XLargeBags.max()-df.XLargeBags.min())
 print()
 
 #Finding the Frequency of different things in the dataset
-print("The frequency of avocado type:")
+print("The frequency of avocado types:")
 print(df['type'].value_counts())
 print()
-print("The frequency of region:")
+print("The frequency of regions:")
 print(df['region'].value_counts())
+
+#Extra
+import matplotlib.pyplot as plt 
+
+x = []
+y = []
+
+with open('avocado.csv', 'r') as csvfile:
+  plots = csv.reader(csvfile, delimiter = ",")
+  
+for row in plots:
+  x.append(row[12])
+  y.append(row[2])
+
+plt.plot(x,y)
+plt.xlabel('Average Price')
+plt.ylabel('Year')
+plt.title('Avocado prices in different years')
+plt.legend()
+plt.show()
